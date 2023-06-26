5b40d2dc7a37cad74ebe18b811d094ecf0454c9d
Simon Damerow <simon.damerow@student.uibk.ac.at>
2023-06-26 10:57:17 +0200

    Simon: Wettervorhersage von Niederösterreich kopiert

--- main.js
+++ main.js
@@ -1,56 +1,144 @@
+/* Code von Vienna mobile Beispiel */
+
 // Zentrum Karte Objekt
-let noeMitte = {
-    lat: 48.27032985615784,
-    lng: 15.764989268344962,
-    title: "Max-Schubert-Warte, Niederösterreich"
-};
+let stpolten = {
+    lat: 48.33001133291213,
+    lng: 16.060959034595086,
+    title: "St. Pölten, Niederösterreich"
+}
+
 
 // Karte initialisieren und Fullscreen Control 
 let map = L.map("map", {
     fullscreenControl: true
 }).setView([
-    noeMitte.lat, noeMitte.lng
-], 8.5);
+    stpolten.lat, stpolten.lng
+], 7.5);
+
 
 // thematische Layer
 let themaLayer = {
-    kampThayaMarch: L.featureGroup(),//https://www.bergfex.at/sommer/niederoesterreich/touren/fernradweg/11709,kamp-thaya-march-radroute/
-    piestingtal: L.featureGroup(),//https://www.bergfex.at/sommer/niederoesterreich/touren/fernradweg/17716,piestingtal-radweg/
-    thayarunde: L.featureGroup(),//https://www.bergfex.at/sommer/niederoesterreich/touren/fernradweg/84734,thayarunde-waldviertel/
-    traisental: L.featureGroup(),//https://www.bergfex.at/sommer/niederoesterreich/touren/fernradweg/17634,traisental-radweg/
-    triestingGoelsental: L.featureGroup(),//https://www.bergfex.at/sommer/niederoesterreich/touren/fernradweg/11703,triesting-goelsental-radweg/
-    triestingau: L.featureGroup(),//https://www.outdooractive.com/r/1366729
-    ybbstal: L.featureGroup(),//https://www.outdooractive.com/r/10654578
+    kampThayaMarch: L.featureGroup(),
+    piestingtal: L.featureGroup(),
+    thayarunde: L.featureGroup(),
+    traisental: L.featureGroup(),
+    triestingGoelsental: L.featureGroup(),
+    triestingau: L.featureGroup(),
+    ybbstal: L.featureGroup(),
+    eurovelo6: L.featureGroup(),
+    eurovelo9: L.featureGroup(),
+    eurovelo13: L.featureGroup(),
     forecast: L.featureGroup(),
-    //badeseen: L.featureGroup()
-};
+}
+
 
 // Hintergrundlayer 
-let layerControl = L.control.layers({
-    "BasemapÖsterreich": L.tileLayer.provider("BasemapAT.grau").addTo(map),
-    "StamenB/W": L.tileLayer.provider("Stamen.TonerLite"),
+//noch den schöneren von der Hauptkarte einfügen, wenn wir das geschafft haben 
+let eGrundkarteWien = L.control.layers({
+    "OpenStreetMap": L.tileLayer.provider("OpenStreetMap.DE").addTo(map),
+    "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
     "CycleTrails": L.tileLayer.provider("CyclOSM"),
 }, {
-    "Kamp-Thaya-March-Radweg": themaLayer.kampThayaMarch.addTo(map),
+    "Unten und Oben": themaLayer.kampThayaMarch.addTo(map),
     "Piestingtal-Radweg": themaLayer.piestingtal.addTo(map),
     "Thayarunde": themaLayer.thayarunde.addTo(map),
     "Traisental-Radweg": themaLayer.traisental.addTo(map),
     "Triesting-Gölsental-Radweg": themaLayer.triestingGoelsental.addTo(map),
-    "Triestingau-Radweg": themaLayer.triestingau.addTo(map),
-    "Ybbstal-Radweg": themaLayer.ybbstal.addTo(map),
     "Wettervorhersage MET Norway": themaLayer.forecast,
-    //"Badeseen": themaLayer.badeseen
 }).addTo(map);
 
-// Layer beim Besuch auf der Seite ausklappen
-layerControl.expand();
 
-// Instanz Leaflet MiniMap
-var miniMap = new L.Control.MiniMap(
-    L.tileLayer.provider("BasemapAT.basemap"), {
-    toggleDisplay: true,
-}
-).addTo(map);
+var gpx = './data/niederoesterreich/kamp_thaya_march.gpx';
+new L.GPX(gpx, { async: true }, {
+    //Polylinien stylen funktioniert noch nicht, marker ausschalten auch nicht
+    polyline_options: [{
+        color: `#76eec6`,
+        opacity: 0.75,
+        weight: 3
+    }, {
+        color: `#76eec6`,
+        opacity: 0.75,
+        weight: 3
+    }]
+}, {
+    marker_options: {
+        startIconUrl: false,
+        endIconUrl: false,
+        shadowUrl: false
+    }
+}).on('loaded', function (e) {
+    //   map.fitBounds(e.target.getBounds());
+}).addTo(themaLayer.kampThayaMarch);
+
+
+var gpx = './data/5_Unten-amp-Oben.gpx';
+new L.GPX(gpx, { async: true }).on('loaded', function (e) {
+    //   map.fitBounds(e.target.getBounds());
+}).addTo(themaLayer.piestingtal);
+
+var gpx = './data/niederoesterreich/thayarunde.gpx';
+new L.GPX(gpx, { async: true }).on('loaded', function (e) {
+    //   map.fitBounds(e.target.getBounds());
+}).addTo(themaLayer.thayarunde);
+
+var gpx = './data/niederoesterreich/traisentalweg.gpx';
+new L.GPX(gpx, { async: true }).on('loaded', function (e) {
+    //   map.fitBounds(e.target.getBounds());
+}).addTo(themaLayer.traisental);
+
+var gpx = './data/niederoesterreich/triesting_goelsental.gpx';
+new L.GPX(gpx, { async: true }).on('loaded', function (e) {
+    //   map.fitBounds(e.target.getBounds());
+}).addTo(themaLayer.triestingGoelsental);
+
+var gpx = './data/niederoesterreich/triestingau.gpx';
+new L.GPX(gpx, { async: true }).on('loaded', function (e) {
+    //   map.fitBounds(e.target.getBounds());
+}).addTo(themaLayer.triestingau);
+
+//Eurovelos erscheinen noch nicht
+
+// Marker der größten Städte
+const STAEDTE = [
+    {
+        title: "St. Pölten, Niederösterreich",
+        lat: 48.18735,
+        lng: 15.64139,
+        wikipedia: "https://de.wikipedia.org/wiki/St._P%C3%B6lten"//Links raus oder anpassen?
+    },
+    {
+        title: "Tulln",
+        lat: 48.33001133291213,
+        lng: 16.060959034595086,
+        wikipedia: "https://de.wikipedia.org/wiki/Wien" //Links raus oder anpassen?
+    },
+    {
+        title: "Krems a.d. Donau",
+        lat: 48.41022698533108,
+        lng: 15.60382006192799,
+        wikipedia: "https://de.wikipedia.org/wiki/Eisenstadt"//Links raus oder anpassen?
+    },
+    {
+        title: "Baden bei Wien",
+        lat: 48.0024595018188,
+        lng: 16.230795040395048,
+        wikipedia: "https://de.wikipedia.org/wiki/Eisenstadt"//Links raus oder anpassen?
+    },
+]
+
+for (let stadt of STAEDTE) {
+    //Marker für den Stopp
+    let marker = L.marker([stadt.lat, stadt.lng])
+        .addTo(map)
+        .bindPopup(`${stadt.title} <br>
+    <a href="${stop.wikipedia}">Wikipedia</a>
+    `)
+};
+
+// Maßstab
+L.control.scale({
+    imperial: false,
+}).addTo(map);
 
 //Geolocation
 map.locate({
@@ -118,291 +206,6 @@ map.on("click", function (evt) {
     showForecast(url, evt.latlng);
 });
 
-//GPX-Tracks
-//Kamp-Thaya-March
-var gpx = './data/niederoesterreich/kamp_thaya_march.gpx';
-let kamp = new L.GPX(gpx, {
-    polyline_options: {
-        color: '#FFD700',
-        opacity: 0.75,
-        weight: 3
-    },
-    marker_options: {
-        startIconUrl: "icons/tab_cycle.png",
-        endIconUrl: false,
-        shadowUrl: false,
-        wptIconUrls: false
-    }
-}).addTo(themaLayer.kampThayaMarch);
-
-// GPX Track visualisieren aus https://raruto.github.io/leaflet-elevation/
-kamp.on("click", function (evt) {
-    let controlElevation = L.control.elevation({
-        time: false,
-        elevationDiv: "#profile",
-        height: 300,
-        theme: "kamp-thaya"
-    }).addTo(map);
-    // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
-    controlElevation.load("./data/niederoesterreich/kamp_thaya_march.gpx")
-});
-
-
-//Piestingtal
-var gpx = './data/niederoesterreich/piestingtal.gpx';
-let piesting = new L.GPX(gpx, {
-    polyline_options: {
-        color: '#EEEE00',
-        opacity: 0.75,
-        weight: 3
-    },
-    marker_options: {
-        startIconUrl: "icons/tab_cycle.png",
-        endIconUrl: false,
-        shadowUrl: false,
-        wptIconUrls: false
-    }
-}).addTo(themaLayer.piestingtal);
-// GPX Track visualisieren aus https://raruto.github.io/leaflet-elevation/
-piesting.on("click", function (evt) {
-    let controlElevation = L.control.elevation({
-        time: false,
-        elevationDiv: "#profile",
-        height: 300,
-        theme: "kamp-thaya"
-    }).addTo(map);
-    // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
-    controlElevation.load("./data/niederoesterreich/piestingtal.gpx")
-});
-
-//Thayarunde
-var gpx = './data/niederoesterreich/thayarunde.gpx';
-let thaya = new L.GPX(gpx, {
-    polyline_options: {
-        color: '#FFEBCD',
-        opacity: 0.75,
-        weight: 3
-    },
-    marker_options: {
-        startIconUrl: "icons/tab_cycle.png",
-        endIconUrl: false,
-        shadowUrl: false,
-        wptIconUrls: false
-    }
-}).addTo(themaLayer.thayarunde);
-// GPX Track visualisieren aus https://raruto.github.io/leaflet-elevation/
-thaya.on("click", function (evt) {
-    let controlElevation = L.control.elevation({
-        time: false,
-        elevationDiv: "#profile",
-        height: 300,
-        theme: "kamp-thaya"
-    }).addTo(map);
-    // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
-    controlElevation.load("./data/niederoesterreich/thayarunde.gpx")
-});
-
-//Traisentalweg
-var gpx = './data/niederoesterreich/traisentalweg.gpx';
-let traisen = new L.GPX(gpx, {
-    polyline_options: {
-        color: '#FFFACD',
-        opacity: 0.75,
-        weight: 3
-    },
-    marker_options: {
-        startIconUrl: "icons/tab_cycle.png",
-        endIconUrl: false,
-        shadowUrl: false,
-        wptIconUrls: false
-    }
-}).addTo(themaLayer.traisental);
-// GPX Track visualisieren aus https://raruto.github.io/leaflet-elevation/
-traisen.on("click", function (evt) {
-    let controlElevation = L.control.elevation({
-        time: false,
-        elevationDiv: "#profile",
-        height: 300,
-        theme: "kamp-thaya"
-    }).addTo(map);
-    // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
-    controlElevation.load("./data/niederoesterreich/traisentalweg.gpx")
-});
-
-//Triesting Gölsental
-var gpx = './data/niederoesterreich/triesting_goelsental.gpx';
-let triesting = new L.GPX(gpx, {
-    polyline_options: {
-        color: '#FFB90F',
-        opacity: 0.75,
-        weight: 3
-    },
-    marker_options: {
-        startIconUrl: "icons/tab_cycle.png",
-        endIconUrl: false,
-        shadowUrl: false,
-        wptIconUrls: false
-    }
-}).addTo(themaLayer.triestingGoelsental);
-// GPX Track visualisieren aus https://raruto.github.io/leaflet-elevation/
-triesting.on("click", function (evt) {
-    let controlElevation = L.control.elevation({
-        time: false,
-        elevationDiv: "#profile",
-        height: 300,
-        theme: "kamp-thaya"
-    }).addTo(map);
-    // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
-    controlElevation.load("./data/niederoesterreich/triesting_goelsental.gpx")
-});
-
-//Triestingau
-var gpx = './data/niederoesterreich/triestingau.gpx';
-let triestingau = new L.GPX(gpx, {
-    polyline_options: {
-        color: '#B8860B',
-        opacity: 0.75,
-        weight: 3
-    },
-    marker_options: {
-        startIconUrl: "icons/tab_cycle.png",
-        endIconUrl: false,
-        shadowUrl: false,
-        wptIconUrls: false
-    }
-}).addTo(themaLayer.triestingau);
-// GPX Track visualisieren aus https://raruto.github.io/leaflet-elevation/
-triestingau.on("click", function (evt) {
-    let controlElevation = L.control.elevation({
-        time: false,
-        elevationDiv: "#profile",
-        height: 300,
-        theme: "kamp-thaya"
-    }).addTo(map);
-    // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
-    controlElevation.load("./data/niederoesterreich/triestingau.gpx")
-});
-
-//Ybbstalweg
-var gpx = './data/niederoesterreich/ybbstalradweg.gpx';
-let ybbs = new L.GPX(gpx, {
-    polyline_options: {
-        color: '#EEDD82',
-        opacity: 0.75,
-        weight: 3
-    },
-    marker_options: {
-        startIconUrl: "icons/tab_cycle.png",
-        endIconUrl: false,
-        shadowUrl: false,
-        wptIconUrls: false
-    }
-}).addTo(themaLayer.ybbstal);
-// GPX Track visualisieren aus https://raruto.github.io/leaflet-elevation/
-ybbs.on("click", function (evt) {
-    let controlElevation = L.control.elevation({
-        time: false,
-        elevationDiv: "#profile",
-        height: 300,
-        theme: "kamp-thaya"
-    }).addTo(map);
-    // Load track from url (allowed data types: "*.geojson", "*.gpx", "*.tcx")
-    controlElevation.load("./data/niederoesterreich/ybbstalradweg.gpx")
-});
-
-//Funktion implementieren für die GPX-Tracks
-/*async function gpxTracks(gpx) {
-    let routenFarben = {//Gelbtöne von https://www.farb-tabelle.de/de/farbtabelle.htm#yellow
-        "Ybbstalradweg": "#EEDD82", //BlanchedAlmond 
-        "Triestingau-Radweg": "#B8860B", //DarkGoldenrod
-        "Triesting-Gölsental-Radweg": "#FFB90F", //DarkGoldenrod1
-        "Traisentalweg": "#FFFACD", //LemonChiffon
-        "Thayarunde Waldviertel": "#FFEBCD", //LightGo.denrod
-        "Piestingtal-Radweg": "#EEEE00", //yellow2
-        "Kamp-Thaya-March-Radroute": "#FFD700", //gold
-    };
-    let zuordnungLayer = {
-        "Ybbstalradweg": "themaLayer.kampThayaMarch",
-        "Piestingtal-Radweg": "themaLayer.piestingtal",
-        "Thayarunde": "themaLayer.thayarunde",
-        "Traisental-Radweg": "themaLayer.traisental",
-        "Triesting-Gölsental-Radweg": "themaLayer.triestingGoelsental",
-        "Triestingau-Radweg": "themaLayer.triestingau",
-        "Ybbstal-Radweg": "themaLayer.ybbstal"
-    };
-    new L.GPX(gpx, {
-        polyline_options: function (feature) {
-            return {
-                color: routenFarben[feature.properties.Name],//der Zugriff auf die Farben funktioniert noch nicht!
-                opacity: 0.75,
-                weight: 3
-            };
-        },
-        marker_options: {
-            startIconUrl: false,
-            endIconUrl: false,
-            shadowUrl: false,
-            wptIconUrls: false
-        },
-    }).on('loaded', function (e) {
-        //   map.fitBounds(e.target.getBounds());
-    }).addTo(themaLayer);//hier noch den richtigen Themalayern zuordnen!
-}
-
-gpxTracks("data/niederoesterreich/kamp_thaya_march.gpx");
-gpxTracks("data/niederoesterreich/piestingtal.gpx");
-gpxTracks("data/niederoesterreich/thayarunde.gpx");
-gpxTracks("data/niederoesterreich/traisentalweg.gpx");
-gpxTracks("data/niederoesterreich/triesting_goelsental.gpx");
-gpxTracks("data/niederoesterreich/triestinggau.gpx");
-gpxTracks("data/niederoesterreich/ybbstalradweg.gpx");
-*/
-
-//Farben und Themalayer zuordnen! Popups für die Tracks erstellen bei Klick (wie in start repo)
-
-
-
-
-// Marker der größten Städte
-const STAEDTE = [
-    {
-        title: "St. Pölten, Niederösterreich",
-        lat: 48.18735,
-        lng: 15.64139,
-        wikipedia: "https://de.wikipedia.org/wiki/St._P%C3%B6lten"
-    },
-    {
-        title: "Tulln",
-        lat: 48.33001133291213,
-        lng: 16.060959034595086,
-        wikipedia: "https://de.wikipedia.org/wiki/Tulln_an_der_Donau"
-    },
-    {
-        title: "Krems a.d. Donau",
-        lat: 48.41022698533108,
-        lng: 15.60382006192799,
-        wikipedia: "https://de.wikipedia.org/wiki/Krems_an_der_Donau"
-    },
-    {
-        title: "Baden bei Wien",
-        lat: 48.0024595018188,
-        lng: 16.230795040395048,
-        wikipedia: "https://de.wikipedia.org/wiki/Baden_(Nieder%C3%B6sterreich)"
-    },
-]
-
-for (let stadt of STAEDTE) {
-    L.marker([stadt.lat, stadt.lng])
-        .addTo(map)
-        .bindPopup(`<b>${stadt.title}</b> <br>
-        <a href="${stadt.wikipedia}">Wikipedia</a>
-    `)
-};
-
-// Maßstab
-L.control.scale({
-    imperial: false,
-}).addTo(map);
 
 // //GPX-Track visualisieren -> Höhenprofile (es sind noch nicht alle)
 // let controlElevation = L.control.elevation({
@@ -458,28 +261,42 @@ L.control.scale({
 // }).addTo(themaLayer.route);
 // controlElevation5.load("data/niederoesterreich/traisentalweg.gpx")
 
-//Badegewässer einblenden -> Daten fehlen noch
-/*async function showLakes(url) {
-    let response = await fetch(url);
-    let jsondata = await response.json();
-    //console.log(response, jsondata);
-    L.geoJSON(jsondata, {
-        style: function (feature) {
-            return {
-                color: "#0074D9",
-                weight: 1,
-                fillOpacity: 0.1,
-                opacity: 0.4
-            };
-        },
-        onEachFeature: function (feature, layer) {
-            let prop = feature.properties;
-            layer.bindPopup(`
-            <h4>Adresse ${prop.ADRESSE}</h4>
-            <p><i class="fa-sharp fa-solid fa-clock"></i> ${prop.ZEITRAUM || "dauerhaft"}</p>
-            <p><i class="fa-sharp fa-solid fa-circle-info"></i> ${prop.AUSN_TEXT || "keine Ausnahmen"}</p>
-            `)
-        }
-    }).addTo(themaLayer.badeseen);
+//Kommentare aus der start-Seite
+/* Pulldownmenü Code
+//Pulldown für Navigation
+let pulldown = document.querySelector("#pulldown");
+for (let etappe of ETAPPEN) {
+    //console.log(etappe);
+    let status = "";
+    if (etappe.nr == "20") {
+        status = "selected";
+    }
+    pulldown.innerHTML += `<option ${status} value="${etappe.user}">Etappe ${etappe.nr}: ${etappe.etappe}</option>`
 }
-showLakes("");*/
+
+// auf Änderungen im Pulldown reagieren
+pulldown.onchange = function(evt) {
+    //console.log(pulldown.value);
+    let url = `https://${pulldown.value}.github.io/biketirol`;
+    //console.log(url);
+    window.location.href = url;
+}
+*/
+
+/*
+let circle = L.circle([0, 0], 0).addTo(map);
+let marker = L.marker([0, 0], 0).addTo(map);
+
+map.on('locationfound', function onLocationFound(evt) {
+    console.log(evt);
+    let radius = Math.round(evt.accuracy);
+    marker.setLatLng(evt.latlng);
+    marker.bindTooltip(`You are within ${radius} meters from this point`).openTooltip();
+    circle.setLatLng(evt.latlng);
+    circle.setRadius(radius);
+});
+
+map.on('locationerror', function onLocationError(evt) {
+    alert(evt.message);
+});
+*/
